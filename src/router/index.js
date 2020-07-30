2946dc6d76c24cd10cc76ecca932bfc2666ac379
WilliamCadenas21 <williamcadenas21@gmail.com>
2020-07-30 12:29:22 -0500

    new componets and finish the global styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,41 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import SignIn from "../views/SignInFlow/SignIn.vue";
+import Recover from "../views/SignInFlow/Recover.vue";
+import Request from "../views/SignInFlow/Request.vue";
+
+
+
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home
   },
   {
     path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    name: "about",
+    component: About
+  },
+  {
+    path: "/signin",
+    name: "signin",
+    component: SignIn
+  },
+  {
+    path: "/recover",
+    name: "recover",
+    component: Recover
+  },
+  {
+    path: "/request",
+    name: "request",
+    component: Request
   }
 ];
 
---
+++ src/views/SignInFlow/SignIn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="login">
+      <img src="@/assets/logoOne.png" />
+      <h4>Sign into Design+Code HQ</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Signin"
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 100vh;
+}
+.login {
+}
+
+h4 {
+  margin: 0;
+  font-size: 24px;
+  line-height: 34px;
+  /* identical to box height */
+  text-align: center;
+  /* white */
+  color: #ffffff;
+}
+</style>
